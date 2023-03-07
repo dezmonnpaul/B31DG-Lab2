2818f04855baeb8e871c22f01922a7f45a02e23a
LUNARWOLF94 <61485783+dezmonnpaul@users.noreply.github.com>
2023-03-07 16:52:57 +0000

    Task02 Update
    Updated Task2 Implementation, yet to test

--- Task01/Task02.ino
+++ Task01/Task02.ino
@@ -2,17 +2,39 @@
 #define In2 10
 
 int Freq02;
-int Signal2;
+bool Signal2;
+const int Task2MinFreq=333;
+const int Task2MaxFreq=1000;
+int Task2MaxPeriod=1/(2*(Task2MinFreq))*1000000;
+int Task2MinPeriod=1/(2*(Task2MaxFreq))*1000000;
 
 
 void Task2(){
-    Freq02=0;
-    if(digitalRead(In2)){
-        unsigned long time02=micros();
-    };
+    Signal2=digitalRead(In2);
+    unsigned long time02=0;
+    bool status02=true;
+    int counter=0;
+    while(status02){
+    if(digitalRead(In2)!=Signal2&&counter==0){
+        time02=micros();
+        counter=1;
+    }
+    else if(digitalRead(In2)!=Signal2&&counter==1){
+      if(micros()-time02>Task2MaxPeriod){
+        time02=0;
+        status02=false;
+      }
+    }
+    else if(digitalRead(In2)==Signal2&&counter==1){
+        time02=micros()-time02;
+        if(time02<Task2MinPeriod){
+        time02=0;
+        }
+        status02=false;
+    }
+  }
+};
 
-
-}
 
 void setup() {
   Serial.begin(9600);
