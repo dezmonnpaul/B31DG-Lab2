4b2fd1e2e624ea1180b5245b26084efb1daf4d48
LUNARWOLF94 <61485783+dezmonnpaul@users.noreply.github.com>
2023-03-07 16:56:39 +0000

    Task3Created
    Created Task3 Code based on Task2, yet to test

---
+++ Task01/Task03.ino
@@ -0,0 +1,49 @@
+
+#define In3 10
+
+int Freq03;
+bool Signal3;
+const int Task3MinFreq=333;
+const int Task3MaxFreq=1000;
+int Task3MaxPeriod=1/(2*(Task3MinFreq))*1000000;
+int Task3MinPeriod=1/(2*(Task3MaxFreq))*1000000;
+
+
+void Task3(){
+    Signal3=digitalRead(In3);
+    unsigned long time03=0;
+    bool status03=true;
+    int counter=0;
+    while(status03){
+    if(digitalRead(In3)!=Signal3&&counter==0){
+        time03=micros();
+        counter=1;
+    }
+    else if(digitalRead(In3)!=Signal3&&counter==1){
+      if(micros()-time03>Task3MaxPeriod){
+        time03=0;
+        status03=false;
+      }
+    }
+    else if(digitalRead(In3)==Signal3&&counter==1){
+        time03=micros()-time03;
+        if(time03<Task3MinPeriod){
+        time03=0;
+        }
+        status03=false;
+    }
+  }
+};
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long time=micros();
+  Task3();
+  time=micros()-time;
+  Serial.println(time);
+  //Max time 286us, expected 280us
+}
